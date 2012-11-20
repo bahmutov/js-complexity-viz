fc4d9d65cf6f4db791e6f6d773fe3007e57cbb03
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2012-11-19 21:41:12 -0500

    grabbing all js files recursively

---
+++ js-complexity-viz.js
@@ -0,0 +1,63 @@
+var fs = require("fs");
+var path = require("path");
+
+var optimist = require("optimist");
+var args = optimist.usage("Visualize JS files complexity.\nUsage: $0")
+		.default({
+			help: 0,
+			path: ".",
+			log: 1
+		}).alias('h', 'help').alias('p', 'path')
+		.string("path")
+		.describe("path", "input folder with JS files")
+		.describe("log", "logging level: 0 - debug, 1 - info")
+		.argv;
+
+if (args.h || args.help) {
+	optimist.showHelp();
+	process.exit(0);
+}
+
+var log = require("custom-logger");
+var logMode = (typeof args.l === "number" ? args.l : 1);
+console.log("log mode", logMode);
+log["new"]({
+	debug: { level: 0, event: "debug", color: "yellow" },
+	log: { level: 1, event: "log" },
+	info: { level: 1, event: "info" },
+	warn: { level: 2, event: "warn", color: "orange" },
+	error: { level: 3, event: "error", color: "red" }
+});
+log.config({
+	level: logMode
+});
+global.log = log;
+
+console.assert(args.path, "empty path");
+log.info("looking for js files in folder", args.path);
+
+var js = /\.js$/i;
+
+var allJsFiles = [];
+function collectJsFiles(folder) {
+	var files = fs.readdirSync(folder);
+	files.forEach(function(filename) {
+		if (filename.match(js)) {
+			filename = path.resolve(filename);
+			allJsFiles.push(filename);
+		} else {
+			try {
+				var stats = fs.lstatSync(filename);
+				if (stats.isDirectory()) {
+					collectJsFiles(filename);
+				}
+			}
+			catch (e) {}
+		}
+	});	
+}
+collectJsFiles(args.path);
+log.debug("found", allJsFiles.length, "js files");
+log.debug(allJsFiles);
+
+
---
+++ test/filenames.js
@@ -0,0 +1,19 @@
+var js = /\.js$/i;
+
+test("detect js file", function () {
+	ok("foo.js".match(js), "foo.js is js file");
+	ok("boo.js".match(js), "boo.js is js file");
+	ok("js.js".match(js), "js.js is js file");
+});
+
+test("detect JS file", function () {
+	ok("foo.JS".match(js), "foo.JS is js file");
+	ok("boo.JS".match(js), "boo.JS is js file");
+	ok("js.JS".match(js), "js.JS is js file");
+});
+
+test("not JS file", function () {
+	ok(!"JS".match(js), "JS is NOT js file");
+	ok(!"js".match(js), "js is NOT js file");
+	ok(!"js.foo".match(js), "js.foo is NOT js file");
+});
